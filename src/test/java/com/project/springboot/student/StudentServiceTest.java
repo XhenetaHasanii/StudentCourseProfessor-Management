a17a2ce7c5eaa2e33afdd90773aabee4222f9f26
XhenetaHasanii <xheneta.hasanni@gmail.com>
2023-10-22 14:38:41 +0200

    Tested the addStudent method in the StudentService class using Mockito

--- src/test/java/com/project/springboot/student/StudentServiceTest.java
+++ src/test/java/com/project/springboot/student/StudentServiceTest.java
@@ -1,10 +1,15 @@
 package com.project.springboot.student;
+import com.project.springboot.course.Course;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.assertj.core.api.Assertions.*;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
@@ -18,16 +23,27 @@ class StudentServiceTest {
         underTest=new StudentService(studentRepository);
     }
     @Test
-    @Disabled
     void addStudent() {
+        //given
+        Set<Course> courses=new HashSet<>();
+        Student student=new Student(1L,"Xheneta","Hasani","xheneta.h@gmail.com",courses);
+        //when
+        underTest.addStudent(student);
+        //then
+        ArgumentCaptor<Student> studentArgumentCaptor=ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+        Student studentCaptorValue=studentArgumentCaptor.getValue();
+        assertThat(studentCaptorValue).isEqualTo(student);
+
     }
 
     @Test
+    @Disabled
     void canGetAllStudents() {
          //when
         underTest.getAllStudents();
-        //that
-        verify(studentRepository).findAll();
+        //then
+    verify(studentRepository).findAll();
     }
 
     @Test
